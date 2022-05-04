346a3747d3e7335354506e46873eaa4a79ffe76f
Oscar Espinosa <oscarzx@gmail.com>
2022-05-03 21:59:30 -0500

    Add products to cart

--- Shopping/Shopping/Data/DataContext.cs
+++ Shopping/Shopping/Data/DataContext.cs
@@ -17,6 +17,7 @@ namespace Shopping.Data
         public DbSet<ProductCategory> ProductCategories { get; set; }
         public DbSet<ProductImage> ProductImages { get; set; }
         public DbSet<State> States { get; set; }
+        public DbSet<TemporalSale> TemporalSales { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
